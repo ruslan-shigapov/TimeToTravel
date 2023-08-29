75719af92072bf38ba2747db62b2014deb0e2921
Ruslanios22 <ilgamovich@gmail.com>
2023-08-29 16:59:22 +0300

    Added customized project

---
+++ TimeToTravel/TimeToTravel/SceneDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  SceneDelegate.swift
+//  TimeToTravel
+//
+//  Created by Руслан Шигапов on 29.08.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene,
+               willConnectTo session: UISceneSession,
+               options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+    }
+}
+
---
+++ TimeToTravel/TimeToTravel/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  TimeToTravel
+//
+//  Created by Руслан Шигапов on 29.08.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+}
+
